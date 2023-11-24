83e4b9261104908011ec1392d4766e4afb00a15c
AVSChaitanya <123075354+AVSChaitanya@users.noreply.github.com>
2023-11-24 12:32:01 +0530

    hi
    hi

--- DigiXpenseAPI/app/main.py
+++ DigiXpenseAPI/app/main.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             """
         )
         uvicorn.run(
-            app=f'{Path(__file__).stem}:apps',
+            app=f'{Path(__file__).stem}:app',
             host=settings.UVICORN_HOST,
             port=settings.UVICORN_PORT,
             reload=settings.UVICORN_RELOAD,
